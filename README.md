79754e079fa332ef955b471013db97abc03cfc0c
OpsCognita <raki.nsv@gmail.com>
2025-06-23 22:09:54 +0200

    inital files

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the repository the AI will modify.
---
+++ infra/main.tf
@@ -0,0 +1,10 @@
+resource "aws_eks_node_group" "general_purpose_nodes" {
+  cluster_name    = "my-cluster"
+  node_group_name = "general_purpose-nodes"
+  instance_types  = ["t2.medium"]
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+}
