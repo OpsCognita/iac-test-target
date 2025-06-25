010b6dfd56cd8645c0e6ebb5ab372574b0a353c6
OpsCognita <raki.nsv@gmail.com>
2025-06-25 14:17:39 +0200

    Update instance type for general purpose nodes

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_node_group" "general_purpose_nodes" {
   cluster_name    = "my-cluster"
   node_group_name = "general_purpose-nodes"
-  instance_types  = ["t2.medium"]
+  instance_types  = ["t3.large"]
   scaling_config {
     desired_size = 2
     max_size     = 3
